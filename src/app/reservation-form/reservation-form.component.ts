1c4538d869f730309fcfa902ce2ce9d48ab41902
petrius <maria.petrius@gmail.com>
2025-08-19 10:48:06 +0200

    introduce a reservation form

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css'],
 })
-export class ReservationFormComponent {}
+export class ReservationFormComponent {
+  reservationForm: FormGroup = new FormGroup({});
+
+  submitReservation() {
+    console.log('>>>>> submitReservation');
+  }
+}
