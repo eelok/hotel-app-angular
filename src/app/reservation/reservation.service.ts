8205abd2553e3873bf02451bfb3375df466282b1
petrius <maria.petrius@gmail.com>
2025-08-21 11:01:38 +0200

    remove localStorage from reservationService

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -8,13 +8,6 @@ import { v4 as uuidv4 } from 'uuid';
 export class ReservationService {
   private reservations: Reservation[] = [];
 
-  constructor() {
-    let storedReservations = localStorage.getItem('reservations');
-    this.reservations = storedReservations
-      ? JSON.parse(storedReservations)
-      : [];
-  }
-
   getReservations(): Reservation[] {
     return this.reservations;
   }
@@ -26,8 +19,6 @@ export class ReservationService {
   createReservation(reservation: Reservation): void {
     reservation.id = uuidv4();
     this.reservations.push(reservation);
-    console.log(reservation);
-    localStorage.setItem('reservations', JSON.stringify(this.reservations));
   }
 
   deleteReservation(id: string): Reservation[] {
@@ -35,13 +26,11 @@ export class ReservationService {
       (reservation) => reservation.id !== id
     );
     this.reservations = fileredReservations;
-    localStorage.setItem('reservations', JSON.stringify(this.reservations));
     return this.reservations;
   }
 
   updateReservation(id: string, undatedReservation: Reservation) {
     let index = this.reservations.findIndex((res) => res.id === id);
     this.reservations[index] = undatedReservation;
-    localStorage.setItem('reservations', JSON.stringify(this.reservations));
   }
 }
